199dba74c9ef8503dbc5bf6647eb5641115414dc
Henrique Domingues <hvdomingues1@gmail.com>
2021-01-07 18:33:50 -0300

    Commit inicial, projeto Spring inicializado

---
+++ src/main/java/com/hvdomingues/cadastroMedico/CadastroMedicoApplication.java
@@ -0,0 +1,13 @@
+package com.hvdomingues.cadastroMedico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastroMedicoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastroMedicoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hvdomingues/cadastroMedico/CadastroMedicoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hvdomingues.cadastroMedico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastroMedicoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
